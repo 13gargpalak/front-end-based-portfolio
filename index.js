c485a2ff0e519759f3dd190539715248c86ff3e9
13gargpalak <129623470+13gargpalak@users.noreply.github.com>
2023-07-03 01:41:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let darkmode = document.getElementById('dark-mode');
+
+darkmode.addEventListener('click', ()=>{
+ 
+        document.querySelector("body").classList.toggle("body-dark-mode")
+        document.querySelector("nav").classList.toggle("nav-dark-mode")
+      //  document.getElementsByName("hr").classList.toggle("hr-dark-mode")
+
+})
+ 
+
+
+ 
